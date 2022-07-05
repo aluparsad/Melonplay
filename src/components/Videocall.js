b3291258a9df100018f16c7bf57f9d2054514b8b
Rohit <rs8987319@gmail.com>
2022-07-05 12:14:10 +0530

    Changes in video call section

--- src/components/Videocall.js
+++ src/components/Videocall.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../sass/video_call.css';
-import { FaHeart, FaMicrophone, FaVideo, FaComment, FaMicrophoneSlash, FaVideoSlash } from 'react-icons/fa';
+import { FaHeart, FaMicrophone, FaVideo, FaComment, FaMicrophoneSlash, FaVideoSlash, FaShareSquare } from 'react-icons/fa';
 
 export default function Video_call() {
     return (
@@ -8,10 +8,46 @@ export default function Video_call() {
             <div className="video-call-container">
                 <div className="video-call-left">
                     <div className="video-call">
-                        <div className="friend friend-1"></div>
-                        <div className="friend friend-2"></div>
-                        <div className="friend friend-3"></div>
-                        <div className="friend friend-4"></div>
+                        <div className="friend friend-1">
+                            <div className="frnd1-icons">
+                                <div className="frnd-mic">
+                                    <FaMicrophone size={20} />
+                                </div>
+                                <div className="frnd-video">
+                                    <FaVideo size={20} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="friend friend-2">
+                            <div className="frnd-icons">
+                                <div className="frnd-mic">
+                                    <FaMicrophone size={20} />
+                                </div>
+                                <div className="frnd-video">
+                                    <FaVideo size={20} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="friend friend-3">
+                            <div className="frnd-icons">
+                                <div className="frnd-mic">
+                                    <FaMicrophone size={20} />
+                                </div>
+                                <div className="frnd-video">
+                                    <FaVideo size={20} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="friend friend-4">
+                            <div className="frnd-icons">
+                                <div className="frnd-mic">
+                                    <FaMicrophone size={20} />
+                                </div>
+                                <div className="frnd-video">
+                                    <FaVideo size={20} />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="buttons">
                         <div className="buttons-section-mic">
@@ -19,6 +55,9 @@ export default function Video_call() {
                         </div>
                         <div className="buttons-section-video">
                             <FaVideoSlash size={28} />
+                        </div>
+                        <div className="buttons-section-screen-share">
+                            <FaShareSquare size={28}/>
                         </div>
                         <button className="end-meeting">End Meeting</button>
                     </div>
@@ -104,23 +143,24 @@ export default function Video_call() {
                     </div>
                     <div className="chatting-section">
                         <div className="chat-heading">
-                            <div className="chat-icon">
+                            {/* <div className="chat-icon">
                                 <FaComment size={18} />
-                            </div>
+                            </div> */}
                             <h2>Chat</h2>
                         </div>
                         <div className="chats">
                             <div className="message message-right">Rohit : How Are You</div>
-                            <div className="message message-left">Dogesh : Iam fine</div>
-                            <div className="message message-right">Rohit : How Are You</div>
-                            <div className="message message-left">Dogesh : Iam fine</div>
-                            <div className="message message-right">Rohit : How Are You</div>
-                            <div className="message message-left">Dogesh : Iam fine</div>
-                            <div className="message message-right">Rohit : How Are You</div>
-                            <div className="message message-left">Dogesh : Iam fine</div>
+                            <div className="message message-left">Dogesh : I am fine</div>
+                            <div className="message message-left">Dogesh : And You Say?</div>
+                            <div className="message message-right">Rohit : I am Good</div>
+                            <div className="message message-left">Dogesh : What are you Doing now?</div>
+                            <div className="message message-right">Rohit : Nothing! just play playing games.</div>
+                            <div className="message message-right">
+                                Rohit : So , Howz You College Going?
+                            </div>
                         </div>
                         <div className="input">
-                            <input type="text" placeholder='Enter Message Here!'/>
+                            <input type="text" placeholder='Enter Message Here!' />
                             <button className="submit">Send</button>
                         </div>
                     </div>
